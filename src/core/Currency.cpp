386bef49a1c96ae62825564df1d06b53f7dc4954
Anatoliy <and1life@yandex.ru>
2025-12-18 20:22:36 +0300

    feat(Currency): added implementation getCode(), getName(), getRate() and setRate()

--- src/core/Currency.cpp
+++ src/core/Currency.cpp
@@ -2,3 +2,23 @@
 
 Currency::Currency(const std::string &code, const std::string &name, double rate) 
     : code_(code), name_(name), rate_(rate) {}
+
+std::string Currency::getCode() const
+{
+    return code_;
+}
+
+std::string Currency::getName() const
+{
+    return name_;
+}
+
+double Currency::getRate() const
+{
+    return rate_;
+}
+
+void Currency::setRate(double rate)
+{
+    rate_ = rate;
+}
