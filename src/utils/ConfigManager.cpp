c07c6500f39970c0c7f29b0a41b8d95ad0cd03c6
Anatoliy <and1life@yandex.ru>
2025-12-19 02:48:14 +0300

    feat(ConfigManager): added implementation loadConfig()

--- src/utils/ConfigManager.cpp
+++ src/utils/ConfigManager.cpp
@@ -16,3 +16,48 @@ const std::vector<CurrencyConfig> &ConfigManager::getCurrencies() const
 {
     return currencies_;
 }
+
+
+void ConfigManager::loadConfig(const std::string &configPath)
+{
+    std::fstream configFile(configPath);
+
+    if (!configFile.is_open())
+    {
+        throw std::runtime_error("Failed to open file: " + configPath);
+    }
+    
+    nlohmann::json configJson;
+
+    try
+    {
+        configFile >> configJson;
+    }
+    catch(const std::exception& e)
+    {
+        throw std::runtime_error("Failed to parse config file: " + std::string(e.what()));
+    }
+    
+    if (configJson.contains("api"))
+    {
+        apiConfig_.url = configJson["api"]["url"];
+        apiConfig_.endpoint = configJson["api"]["endpoint"];
+        apiConfig_.apiKey = configJson["api"]["apiKey"];
+    } else {
+        throw std::runtime_error("Config file does not contain 'api' section");
+    }
+
+    if (configJson.contains("currencies"))
+    {
+        for (const auto& currencyJson : configJson["currencies"])
+        {
+            CurrencyConfig currency;
+            currency.code = currencyJson["code"];
+            currency.name = currencyJson["name"];
+            currency.rate = currencyJson["rate"];
+            currencies_.push_back(currency);
+        }
+    } else {
+        throw std::runtime_error("Config file does not contain 'currencies' section");
+    }
+}
