ffce7523bf20d32e2893bd10d1f91668f6a1f9fc
Anatoliy <and1life@yandex.ru>
2025-12-19 02:27:14 +0300

    feat(ConfigManager): added implementation getApiConfig() and getCurrencies()

--- src/utils/ConfigManager.cpp
+++ src/utils/ConfigManager.cpp
@@ -6,3 +6,13 @@ ConfigManager::ConfigManager(const std::string &configPath)
 {
     loadConfig(configPath);
 }
+
+const ApiConfig &ConfigManager::getApiConfig() const
+{
+    return apiConfig_;
+}
+
+const std::vector<CurrencyConfig> &ConfigManager::getCurrencies() const
+{
+    return currencies_;
+}
