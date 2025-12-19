b2d6a5525058a39410486808ce67c41d3d87c190
Anatoliy <and1life@yandex.ru>
2025-12-19 15:25:27 +0300

    chore(project): updated CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ int main(int argc, char const *argv[])
 
         ExchangeRate exchangeRate(configManager);
 
-        // ExchangeApi api(exchangeRate, configManager);
-        // api.fetchRates();
+        ExchangeApi api(exchangeRate, configManager);
+        api.fetchRates();
 
         double result = exchangeRate.convert("USD", "EUR", 100.0);
         std::cout << "100 USD = " << result << " EUR" << std::endl;
